cd9624d989c862a4048131a01b8a7d47ba537d41
Jeremy Langevin <langevinj832@gmail.com>
2020-11-20 11:55:08 -0500

    useAxios hook created

--- src/hooks.js
+++ src/hooks.js
@@ -1,13 +1,23 @@
 import {useState} from 'react'
+import axios from "axios"
+import uuid from "uuid"
 
 const useFlip = (initialVal=false) => {
-
     const[value, setValue] = useState(initialVal)
     const toggle = () => {
         setValue(oldValue => !oldValue)
     }
-
     return [value, toggle];
 }
 
-export {useFlip}
+function useAxios(url){
+   const [data, setData] = useState([])
+
+   const addCard = async () => {
+        const res = await axios.get(url)
+        setData([...data, {...res.data, id: uuid()}])
+   }
+    return [data, addCard]
+}
+
+export {useFlip, useAxios}
